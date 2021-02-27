db24964cb4cd52b9a0760f8b17bc999883432bf5
Maxim Umblia <umblia34@gmail.com>
2021-02-27 12:36:37 +0700

    fix components

--- src/components/CardsList/CardsList.jsx
+++ src/components/CardsList/CardsList.jsx
@@ -1,16 +1,24 @@
-import {Container,CardColumns } from "react-bootstrap";
-import { connect } from "react-redux";
+import {Container,CardColumns,InputGroup,FormLabel } from "react-bootstrap";
+import { connect,useDispatch } from "react-redux";
 import CardItem from "../Card/Card";
+import orderBy from 'lodash/orderBy'
+import { setFilter } from "../../store/actions/filterCards";
 
 const CardsList = ({items}) => {
-
+    const dispatch = useDispatch(); 
     return (
         <div>
-            <Container>
+            <Container style={{margin:"100px 0 0 0"}}>
+            <InputGroup >
+                <InputGroup.Prepend>
+                <InputGroup.Radio onChange={() => dispatch(setFilter("name"))}  aria-label="Radio button for following text input" />
+                </InputGroup.Prepend>
+                <FormLabel>Sort by filesize</FormLabel>
+            </InputGroup>
                 <CardColumns>
                     {items.splice(0,15).map((item,index) => {
                         return <CardItem  key={index} card={item}/>
-                    })}
+                    })}  
                 </CardColumns>
             </Container>
         </div>
@@ -20,6 +28,15 @@ const mapStateToProps = (state) => {
     return {
       items: state.cards.items,
     }
-  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return{
+        setFilter:(filterBy) => dispatch(setFilter(filterBy))
+    }
+}
+const sortBy = (items,filterBy) =>{
+    return orderBy(items,filterBy)
+}
   
-export default connect(mapStateToProps)(CardsList);
+export default connect(mapStateToProps,mapDispatchToProps)(CardsList);
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,9 +4,7 @@ const Header = () => {
         <Navbar fixed="top" bg="dark" variant="dark">
             <Navbar.Brand href="#home">Navbar</Navbar.Brand>
             <Nav className="mr-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
+
             </Nav>
             <Form inline>
             <FormControl type="text" placeholder="Search" className="mr-sm-2" />
