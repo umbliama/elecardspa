6db96445b10ab14a36e627decb837ad82b658cc1
Maxim Umblia <umblia34@gmail.com>
2021-03-02 21:04:32 +0700

    add scss tree component

--- src/components/Tree/Tree.jsx
+++ src/components/Tree/Tree.jsx
@@ -1,20 +1,21 @@
-
+import "./Tree.scss";
 const Tree = ({data}) => {
     const url ="http://contest.elecard.ru/frontend_data/";
     const uniqueCategories = data.map(item => item.category) 
     const mainCategories = uniqueCategories.filter((item,index) => uniqueCategories.indexOf(item) === index)
+
     return (
-        <ul>
-            Root
-            <ul>
+        <ul className="tree-main__list"> 
+            <li className="tree-main__item"><span  class="tree-main__caret">Root</span></li>
+            <ul className="tree-main__list--nested" >
                 {console.log(mainCategories)}
                 {mainCategories.map((mainCategory,index) => {
                     return (
                         <div>
-                            <li>{mainCategory}</li>
-                            <ul>
+                            <li key={index}><span className="tree-main__caret">{mainCategory}</span></li>
+                            <ul className="tree-main__list--nested">
                             {data.map((item,index) => {
-                                {if (item.category === mainCategory) return <li><img style={{width:"50px"}} src={`${url}${item.image}`} /></li>}
+                                {if (item.category === mainCategory) return <li key={index}><img alt="" style={{width:"50px"}} src={`${url}${item.image}`} /></li>}
 
                             })}  
                         </ul>
