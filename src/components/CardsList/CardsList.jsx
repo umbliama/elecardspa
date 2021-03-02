ebddf5db35e791e090b4b292dc4f510c053d4ee2
Maxim Umblia <umblia34@gmail.com>
2021-03-02 21:06:04 +0700

    refactor cardlist component

--- src/components/CardsList/CardsList.jsx
+++ src/components/CardsList/CardsList.jsx
@@ -1,40 +1,27 @@
-import {Container,CardGroup,InputGroup,FormLabel, Row , Col, CardDeck} from "react-bootstrap";
-import { connect,useDispatch } from "react-redux";
+import {Container, CardDeck} from "react-bootstrap";
+import { connect } from "react-redux";
 import CardItem from "../Card/Card";
 import orderBy from 'lodash/orderBy'
-import { setFilter } from "../../store/actions/filterCards";
 import {setTotalPages } from '../../store/actions/pagesCreator'
 import Tree from "../Tree/Tree";
-import {openTreeView,closeTreeView} from '../../store/actions/treeview'
-import {incPage} from '../../store/actions/pagesCreator'
 
 
-const CardsList = ({items,filterBy,isOpen,perPage}) => {
-    const dispatch = useDispatch(); 
+const CardsList = ({items,filterBy,isOpen,perPage,currentPage}) => {
+    const indexOfLastItem = currentPage * perPage;
+    const indexOfFirstItem = indexOfLastItem - perPage
+    const currentItems = items.slice(indexOfFirstItem,indexOfLastItem)
     if(isOpen) {
         return (
-            <Container style={{margin:"100px 0 0 0"}}>
-                <InputGroup.Radio checked={!isOpen} onChange={() => dispatch(closeTreeView())}  aria-label="Radio button for following text input" />
+            <Container>
                 <Tree style={{margin:"0 0 300px 0"}} data={items} />
             </Container>
         )
     }else {
         return (
             <Container >
-            <InputGroup.Radio checked={isOpen} onChange={() => dispatch(openTreeView())}  aria-label="Radio button for following text input" />
-                <InputGroup >
-                <InputGroup.Prepend>
-                <InputGroup.Radio checked={filterBy === "filesize"} onChange={() => dispatch(setFilter("filesize"))}  aria-label="Radio button for following text input" />
-                <FormLabel>Sort by filesize</FormLabel>
-                <InputGroup.Radio checked={filterBy === "category"} onChange={() => dispatch(setFilter("category"))}  aria-label="Radio button for following text input" />
-                <FormLabel>Sort by category</FormLabel>
-                <InputGroup.Radio checked={filterBy === "timestamp"} onChange={() => dispatch(setFilter("timestamp"))}  aria-label="Radio button for following text input" />
-                <FormLabel>Sort by timestamp</FormLabel>
-                </InputGroup.Prepend>
-            </InputGroup>
-                <CardDeck>
-                    {items.splice(0 + perPage,perPage).map((item,index) => {
-                        return <CardItem  key={index} card={item}/>
+                <CardDeck style={{margin:"200px 0 50px 0"}}>
+                    {sortBy(currentItems,filterBy).map((item,index) => {
+                        return <CardItem keyId={index}  key={index} card={item}/>
                     })}  
                 </CardDeck>
             </Container>
@@ -44,25 +31,22 @@ const CardsList = ({items,filterBy,isOpen,perPage}) => {
 }
 const mapStateToProps = (state) => {
     return {
-      items: sortBy(state.cards.items,state.filter.filterBy),
+      items: state.cards.items,
       filterBy:state.filter.filterBy,
       isOpen:state.treeview.isOpen,
-      perPage:state.pages.perPage
-    }
+      perPage:state.pages.perPage,
+      currentPage:state.pages.currentPage,
+}
 }
 
 //TODO BINDACTIONS
 const mapDispatchToProps = (dispatch) => {
     return{
-        setFilter:(filterBy) => dispatch(setFilter(filterBy)),
         setTotalPages: (totalCount, perPage) => dispatch(setTotalPages(totalCount, perPage)),
-        openTreeView: () => dispatch(openTreeView()),
-        closeTreeView: () => dispatch(closeTreeView()),
-        incPage:() => dispatch(incPage())
     }
 }
 const sortBy = (items,filterBy) =>{
-    return orderBy(items,filterBy,"desc")
+    return orderBy(items,filterBy,"asc")
 }
   
 export default connect(mapStateToProps,mapDispatchToProps)(CardsList);
